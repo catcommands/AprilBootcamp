bf4787593e992f781f8f22464f9ea62a805ec649
JaffarShah <sjeffshah@gmail.com>
2019-04-08 11:17:33 -0400

    Step 2: getAndPrintHTML, Buffered Strings

---
+++ W2D1/HTTPSNodeJS/Step2printBufferedStrings.js
@@ -0,0 +1,58 @@
+var https = require('https'); // We need to set up https here to require data in cmd
+var getData = "";
+
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',// can be any website or link, but in our case it is already given a link
+    path: '/http-examples/step2.html'
+  };
+/* Add your code here */
+https.get(requestOptions, function (response) {
+
+  response.setEncoding('utf8');// is a variable width character encoding: https://en.wikipedia.org/wiki/UTF-8
+
+response.on('data', function(data) {
+getData += data;
+  console.log(getData);
+
+});
+
+response.on('end', function() {
+  console.log('Response stream complete.');
+});
+
+});
+
+}
+console.log(getAndPrintHTML());// we need to console.log to call in the data we are requiring
+
+/*
+Terminal:
+undefined
+
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <title>HTTP Requests - Step 2</title>
+  </head>
+  <body>
+    <pre>
+
+Nicely done. Now you should be seeing a neatly formatted HTML document,
+not a crazy deluge of random chunks.
+All hail the great +=! (or push, if you used an array...)
+
+But we can still do better. This function is not modular.
+
+In the next exercise, we'll pass the requestOptions as a param to the function,
+so the function can be reused for different URLs.
+
+Make sure you commit before you start the next Step!
+
+    </pre>
+  </body>
+</html>
+
+Response stream complete.
+*/
