da744b54fbee2c22a45139f8b974935c5debd8fb
JaffarShah <sjeffshah@gmail.com>
2019-04-07 21:10:45 -0400

    Steps: 4 and 5 with comments

--- Weekend1/https_example.js
+++ Weekend1/https_example.js
@@ -4,18 +4,24 @@ var https = require('https');
 
 console.log('I did it!');
 
+// Terminal: I did it!
+
 //Step 3: Make a simple request
 var options = {
     host: 'www.example.org',
     path: '/'
   };
-  
-//terminal answer: I did it!
 
+// Step 4: read the step 4 and "where's my response?" 
+//and view souce code at https://example.org
 
-  // called by https when the request is made.
-  var callback = function() {
+//Step 5: This response. Modified the car callback function and included response.
+//for step 5, node the file in terminal.
+
+// called by https when the request is made.
+var callback = function(response) {
     console.log('In response handler callback!');
+    console.log('Response: ', response);
   }
   
   console.log("I'm about to make the request!");
