4922701fde3aaaa45ddb4f2c42fae972d2e1ec5a
JaffarShah <sjeffshah@gmail.com>
2019-04-08 12:14:09 -0400

    module testing for step 5

---
+++ W2D1/HTTPSNodeJS/moduletestingStep5.js
@@ -0,0 +1,9 @@
+var https = require('https');
+var getData = "";
+
+var getHTML = require('./Step5Module');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
