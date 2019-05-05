e16fdc002cf3f5a2ef4629bd96b52554989a265d
JaffarShah <sjeffshah@gmail.com>
2019-05-05 14:56:30 -0400

    Add subordinates

--- W5D4/durian-tree.js
+++ W5D4/durian-tree.js
@@ -1,3 +1,10 @@
+const ada = new Employee("Ada", "CEO", 3000000.00);
+
+const craig    = new Employee("Craig", "VP Software", 1000000);
+const arvinder = new Employee("Arvinder", "Chief Design Officer", 1000000);
+const angela   = new Employee("Angela", "VP Retail", 1000000);
+const phil     = new Employee("Phil", "VP Marketing", 1000000);
+
 class Employee {
     constructor(name, title, salary) {
       this.name = name;
@@ -6,4 +13,14 @@ class Employee {
       this.boss = null;
       this.subordinates = [];
     }
+  
+    addSubordinate(subordinate) {
+      this.subordinates.push(subordinate);
+      subordinate.boss = this;
+    }
   }
+
+  ada.addSubordinate(craig);
+  ada.addSubordinate(arvinder);
+  ada.addSubordinate(angela);
+  ada.addSubordinate(phil);
